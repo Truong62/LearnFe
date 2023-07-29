fe449a21813a841958fe7d2839c71478dc198095
NNT <truong06022004@gmail.com>
2023-07-29 17:54:17 +0700

    javascrip

---
+++ jsHoc/intro.js
@@ -0,0 +1,37 @@
+//day1+2 - 29/7/2023
+console.log("Welcome to 30DaysOfJavaScript");
+let firstName = 'nguyen van ngu'
+console.log(firstName)
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+
+const P3I = Math.PI;
+console.log(Math.floor(P3I));
+
+console.log(Math.min(-5, 3, 20, 4, 5, 10));
+console.log(Math.max(-5, 3, 20, 4, 5, 10));
+
+const randNum = Math.random(); // creates random number between 0 to 0.999999
+console.log(randNum);
+
+const num = Math.floor(Math.random() * 21); // creates random number between 0 and 10
+console.log(num);
+
+console.log(Math.LN10);
+// Trigonometry
+Math.sin(0)
+Math.sin(60)
+
+Math.cos(0)
+Math.cos(60)
+
+let string = "   30 Days Of JavaScript   ";
+
+console.log(string);
+console.log(string.trim(" "));
+
+let country = "Finland";
+console.log(country.replace("Fin", "Noman"));
