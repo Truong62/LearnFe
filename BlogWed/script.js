50f499b2a4b3327d0bfdac2bc6f3918f8e2c2ed9
NNT <truong06022004@gmail.com>
2023-09-12 11:14:03 +0700

    update

--- BlogWed/script.js
+++ BlogWed/script.js
@@ -1,3 +1,94 @@
+//day1+2 - 29/7/2023
+console.log("Welcome to 30DaysOfJavaScript");
+let firstName = "nguyen van ngu";
+console.log(firstName);
+console.log(typeof "Asabeneh"); // string
+console.log(typeof 5); // number
+console.log(typeof true); // boolean
+console.log(typeof null); // object type
+console.log(typeof undefined); // undefined
+
+const P3I = Math.PI;
+console.log(Math.floor(P3I));
+
+console.log(Math.min(-5, 3, 20, 4, 5, 10));
+console.log(Math.max(-5, 3, 20, 4, 5, 10));
+
+const randNum = Math.random(); // creates random number between 0 to 0.999999
+console.log(randNum);
+
+const num = Math.floor(Math.random() * 21); // creates random number between 0 and 10
+console.log(num);
+
+console.log(Math.LN10);
+// Trigonometry
+Math.sin(0);
+Math.sin(60);
+
+Math.cos(0);
+Math.cos(60);
+
+let string = "   30 Days Of JavaScript   ";
+
+console.log(string);
+console.log(string.trim(" "));
+
+let country = "Finland";
+console.log(country.replace("Fin", "Noman"));
+
+console.log(
+  "==================================================================="
+);
+
+let challenge = "30 Days Of JavaScript";
+console.log(challenge);
+console.log(challenge.length);
+console.log(challenge.toUpperCase());
+console.log(challenge.toLowerCase());
+console.log(challenge.substring(3, 7));
+console.log(challenge.substring(3));
+console.log(challenge.includes("Script"));
+
+// Tách chuỗi thành mảng bằng phương thức split()
+// Tách chuỗi 30 Days Of JavaScript tại không gian bằng phương thức split()
+// 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' chia chuỗi tại dấu phẩy và thay đổi nó thành một mảng.
+// Thay đổi 30 Days Of JavaScript thành 30 Days Of Python bằng phương thức replace() .
+// Ký tự ở chỉ mục 15 trong chuỗi '30 Days Of JavaScript' là gì? Sử dụng phương thức charAt() .
+// Mã ký tự của J trong chuỗi '30 Days Of JavaScript' bằng charCodeAt() là gì
+// Sử dụng indexOf để xác định vị trí xuất hiện đầu tiên của một trong 30 Days Of JavaScript
+// Sử dụng lastIndexOf để xác định vị trí của lần xuất hiện cuối cùng trong 30 Days Of JavaScript.
+// Sử dụng indexOf để tìm vị trí xuất hiện đầu tiên của từ vì trong câu sau: 'Bạn không thể kết thúc câu bằng vì vì vì vì là một liên từ'
+// Sử dụng lastIndexOf để tìm vị trí xuất hiện cuối cùng của từ vì trong câu sau: 'Bạn không thể kết thúc câu bằng vì vì vì vì là một liên từ'
+// Sử dụng tìm kiếm để tìm vị trí xuất hiện đầu tiên của từ vì trong câu sau: 'Bạn không thể kết thúc một câu với vì vì vì vì là một liên từ'
+// Sử dụng trim() để xóa bất kỳ khoảng trắng ở cuối chuỗi nào ở đầu và cuối chuỗi. Ví dụ: ' 30 Days Of JavaScript '.
+// Sử dụng phương thức startedWith() với chuỗi 30 Days Of JavaScript và biến kết quả thành true
+// Sử dụng phương thức endWith() với chuỗi 30 Days Of JavaScript và biến kết quả thành true
+// Sử dụng phương thức match() để tìm tất cả a trong 30 Days Of JavaScript
+// Sử dụng concat() và hợp nhất '30 Days of' và 'JavaScript' thành một chuỗi duy nhất, '30 Days Of JavaScript'
+// Sử dụng phương thức repeat() để in 30 Days Of JavaScript 2 lần
+console.log(challenge.split());
+console.log(challenge.split(" "));
+s = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+console.log(s.split(", "));
+console.log(challenge.replace("JavaScript", "Python"));
+console.log(challenge.charAt(15));
+console.log(challenge.charCodeAt(11));
+console.log(challenge.indexOf("3"));
+console.log(challenge.lastIndexOf("t"));
+// console.log(challenge.)
+
+var Hello = function () {
+  var nameten = document.getElementById("name").value;
+  var output = "";
+
+  for (var i = 0; i <= 10; i++) {
+    var test = i + " Xin chào " + nameten + "<br>";
+    output += test;
+  }
+
+  document.getElementById("name hello").innerHTML = output;
+};
+
 // const marquee = document.getElementById("marqueeText");
 // const text = marquee.innerHTML;
 
@@ -102,7 +193,6 @@ console.log(styles);
 // let toral = sumInput();
 // console.log("tong cua cac gia trij laf : " + toral);
 
-
 function getMaxSubSum(arr) {
   let maxSum = 0; // Khởi tạo tổng lớn nhất ban đầu
   let currentSum = 0; // Khởi tạo tổng hiện tại ban đầu
@@ -130,30 +220,83 @@ let arr = [1, -2, 3, 4, -9, 6];
 let maxSubSum = getMaxSubSum(arr);
 console.log("Tổng lớn nhất của mảng con liền kề là:", maxSubSum);
 
+// function camelize(str) {
+//   return str
+//     .split("-") // Tách chuỗi thành mảng các từ theo dấu gạch ngang
+//     .map((word, index) => {
+//       // Chuyển đổi các từ (trừ từ đầu tiên) thành dạng camel case
+//       if (index === 0) {
+//         return word; // Giữ nguyên từ đầu tiên
+//       } else {
+//         return word[0].toUpperCase() + word.slice(1);
+//       }
+//     })
+//     .join(""); // Kết hợp lại các từ thành chuỗi mới
+// }
+// console.log(camelize("background-color"))
+// console.log(camelize("list-style-image"));
+// console.log(camelize("-webkit-transition"))
 
-function camelize(str) {
-  return str
-    .split("-") // Tách chuỗi thành mảng các từ theo dấu gạch ngang
-    .map((word, index) => {
-      // Chuyển đổi các từ (trừ từ đầu tiên) thành dạng camel case
-      if (index === 0) {
-        return word; // Giữ nguyên từ đầu tiên
-      } else {
-        return word[0].toUpperCase() + word.slice(1);
-      }
-    })
-    .join(""); // Kết hợp lại các từ thành chuỗi mới
-}
-console.log(camelize("background-color"))
-console.log(camelize("list-style-image"));
-console.log(camelize("-webkit-transition"))
+// function filterRange(arr, a, b){
+//   for(let item = a; a<=b;item++){
+//     arr.push(Number(item))
+//   }
+//   return arr
+// }
+// let arr_7 = [];
+// let filterrange = filterRange(arr_7,1, 10)
+// console.log(arr_7)
 
-function filterRange(arr, a, b){
-  for(let item = a; a<=b;item++){
-    arr.push(Number(item))
+var user = {};
+
+user.name = "David";
+user.surname = "Xuan";
+user.name = "Cafedev";
+console.log(user);
+delete user.name;
+
+function isEmpty(obj) {
+  if (Object.keys(obj).length === 0) {
+    return true;
+  } else {
+    return false;
   }
-  return arr
 }
-let arr_7 = [];
-let filterrange = filterRange(arr_7,1, 10)
-console.log(arr_7)
+// let schedule = {};
+
+// alert( isEmpty(schedule) );
+
+// schedule["8:30"] = "get up";
+
+// alert( isEmpty(schedule) );
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+let total = 0;
+for (let key in salaries) {
+  total += salaries[key];
+}
+salaries.total = total;
+console.log(salaries.total);
+
+function multiplyNumeric(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === "string") {
+      continue;
+    } else if (typeof obj[key] === "number") {
+      obj[key] = obj[key] * 2;
+    }
+  }
+}
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu cafedev",
+};
+
+multiplyNumeric(menu);
+
+console.log(menu);
