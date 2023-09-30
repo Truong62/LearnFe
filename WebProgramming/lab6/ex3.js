c4e9ecb679aab9643015caba8d49ae44c510c1d1
NNT <truong06022004@gmail.com>
2023-09-30 16:56:26 +0700

    update

--- WebProgramming/lab6/ex3.js
+++ WebProgramming/lab6/ex3.js
@@ -1,8 +1,41 @@
+var table = document.getElementById("sampleTable");
+
 function insert_Row() {
-  var table = document.getElementById("sampleTable");
   var row = table.insertRow();
   var cell1 = row.insertCell();
   var cell2 = row.insertCell();
-  cell1.innerHTML = "New Cell1";
-  cell2.innerHTML = "New Cell2";
+  var cell3 = row.insertCell();
+
+  var inputElement1 = document.createElement("input");
+  cell1.appendChild(inputElement1);
+
+  var inputElement2 = document.createElement("input");
+  cell2.appendChild(inputElement2);
+
+  var inputElement3 = document.createElement("input");
+  cell3.appendChild(inputElement3);
 }
+
+var inputNb = document.getElementById("inputNb");
+inputNb.onclick = insert_Row;
+
+var Del = document.getElementById("Del");
+// Del.onclick = function () {
+
+//     const inputNumber = prompt("Dòng muốn xóa: ");
+//     const fomatnb = parseInt(inputNumber) +1;
+//   table.deleteRow(parseInt(inputNumber + 1));
+// };
+
+Del.onclick = function () {
+  const inputNumber = prompt("Dòng muốn xóa: ");
+  const rowIndex = parseInt(inputNumber); 
+  if (!isNaN(rowIndex) && rowIndex >= 0 && rowIndex < table.rows.length) {
+    table.deleteRow(rowIndex + 1);
+  } else {
+    alert(
+      "Vui lòng nhập một số nguyên hợp lệ trong khoảng từ 0 đến " +
+        (table.rows.length - 1)
+    );
+  }
+};
