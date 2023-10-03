b98ac8dcc089dde930c9ef05f4d8d76f1d7dfa7b
NNT <truong06022004@gmail.com>
2023-10-03 21:53:10 +0700

    update

--- WebProgramming/lab6/ex5-6.js
+++ WebProgramming/lab6/ex5-6.js
@@ -1,24 +1,49 @@
-function showError() {
-  const e = document.getElementById("fname").value;
-  if(e = null){
-    alert("ko de trong")
-    return false
+const fnameInput = document.getElementById("fname");
+const errorMessage = document.getElementById("error-fullName");
+
+const fullName = document.getElementById("fname").value;
+const email = document.getElementById("email").value;
+const phone = document.getElementById("phone").value;
+const country = document.getElementById("country").value;
+const comment = document.getElementById("comment").value;
+
+fnameInput.addEventListener("input", function () {
+  if (!fnameInput.checkValidity()) {
+    errorMessage.textContent = "Please enter a valid full name.";
+    errorMessage.style.display = "block";
+  } else {
+    errorMessage.textContent = "";
+    errorMessage.style.display = "none";
   }
-}
+});
+const fnameInputemail = document.getElementById("email");
+const errorEmail = document.getElementById("error-email");
+
+fnameInputemail.addEventListener("input", function () {
+   if (!fullName) {
+     errorMessage.textContent = "Please enter a valid full name.";
+     errorMessage.style.display = "block";
+   } else {
+     errorMessage.textContent = "";
+     errorMessage.style.display = "none";
+   }
+   if (!fnameInputemail.checkValidity()) {
+     errorEmail.textContent = "Please enter a valid email.";
+     errorEmail.style.display = "block";
+   } else {
+     errorEmail.textContent = "";
+     errorEmail.style.display = "none";
+   }
+});
 
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form");
-  
+const selectedGenderInput = document.querySelector(
+  'input[name="gender"]:checked'
+);
 
   form.addEventListener("submit", function (e) {
     e.preventDefault();
-
-    const fullName = document.getElementById("fname").value;
-    const email = document.getElementById("email").value;
-    const phone = document.getElementById("phone").value;
-    const gender = document.querySelector('input[name="gender"]:checked').value;
-    const country = document.getElementById("country").value;
-    const comment = document.getElementById("comment").value;
 
     const showInfor = document.querySelector(".showInfor");
     showInfor.innerHTML = `
