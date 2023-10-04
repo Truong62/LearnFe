1f78714dca19d03a9f60c8e0e3790a0beb0a4693
NNT <truong06022004@gmail.com>
2023-10-04 13:21:21 +0700

    update

--- WebProgramming/lab6/ex5-6.js
+++ WebProgramming/lab6/ex5-6.js
@@ -1,11 +1,11 @@
 const fnameInput = document.getElementById("fname");
 const errorMessage = document.getElementById("error-fullName");
 
-const fullName = document.getElementById("fname").value;
-const email = document.getElementById("email").value;
-const phone = document.getElementById("phone").value;
-const country = document.getElementById("country").value;
-const comment = document.getElementById("comment").value;
+const fullName = document.getElementById("fname");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const country = document.getElementById("country");
+const comment = document.getElementById("comment");
 
 fnameInput.addEventListener("input", function () {
   if (!fnameInput.checkValidity()) {
@@ -20,42 +20,58 @@ const fnameInputemail = document.getElementById("email");
 const errorEmail = document.getElementById("error-email");
 
 fnameInputemail.addEventListener("input", function () {
-   if (!fullName) {
-     errorMessage.textContent = "Please enter a valid full name.";
-     errorMessage.style.display = "block";
-   } else {
-     errorMessage.textContent = "";
-     errorMessage.style.display = "none";
-   }
-   if (!fnameInputemail.checkValidity()) {
-     errorEmail.textContent = "Please enter a valid email.";
-     errorEmail.style.display = "block";
-   } else {
-     errorEmail.textContent = "";
-     errorEmail.style.display = "none";
-   }
+  if (!fullName.checkValidity()) {
+    errorMessage.textContent = "Please enter a valid full name.";
+    errorMessage.style.display = "block";
+  } else {
+    errorMessage.textContent = "";
+    errorMessage.style.display = "none";
+  }
+  if (!fnameInputemail.checkValidity()) {
+    errorEmail.textContent = "Please enter a valid email.";
+    errorEmail.style.display = "block";
+  } else {
+    errorEmail.textContent = "";
+    errorEmail.style.display = "none";
+  }
 });
 
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("form");
-const selectedGenderInput = document.querySelector(
-  'input[name="gender"]:checked'
-);
 
   form.addEventListener("submit", function (e) {
     e.preventDefault();
-
+    const fullNameValue = fullName.value;
+    const emailValue = email.value;
+    const phoneValue = phone.value;
+    const gender = document.querySelector('input[name="gender"]:checked');
+    const countryValue = country.value;
+    const commentValue = comment.value;
     const showInfor = document.querySelector(".showInfor");
-    showInfor.innerHTML = `
-      <h2>Customer Form:</h2>
-      <p><strong>Full Name:</strong> ${fullName}</p>
-      <p><strong>Email:</strong> ${email}</p>
-      <p><strong>Phone:</strong> ${phone}</p>
-      <p><strong>Gender:</strong> ${gender}</p>
-      <p><strong>Country:</strong> ${country}</p>
-      <p><strong>Comment:</strong> ${comment}</p>
-    `;
 
-    form.reset();
+    if (
+      !fullNameValue ||
+      !emailValue ||
+      !phoneValue ||
+      !gender ||
+      !countryValue ||
+      commentValue == ""
+    ) {
+      showInfor.innerHTML =
+        "<p style='color: red;'>error</p>";
+    } else {
+      showInfor.innerHTML = `
+        <h2>Customer Form:</h2>
+        <p><strong>Full Name:</strong> ${fullNameValue}</p>
+        <p><strong>Email:</strong> ${emailValue}</p>
+        <p><strong>Phone:</strong> ${phoneValue}</p>
+        <p><strong>Gender:</strong> ${gender.value}</p>
+        <p><strong>Country:</strong> ${countryValue}</p>
+        <p><strong>Comment:</strong> ${commentValue}</p>
+      `;
+
+      form.reset();
+    }
   });
 });
+
