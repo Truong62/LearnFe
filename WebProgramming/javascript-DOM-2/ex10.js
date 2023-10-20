a4be2528615dad6e1efe9a6c8f3cac3557240cbb
NNT <truong06022004@gmail.com>
2023-10-20 15:48:13 +0700

    update

--- WebProgramming/javascript-DOM-2/ex10.js
+++ WebProgramming/javascript-DOM-2/ex10.js
@@ -19,3 +19,36 @@ function xuLyPhimXuong(event) {
 }
 
 document.addEventListener("keydown", xuLyPhimXuong);
+
+
+
+const populatedCountries = [
+  { name: "China", population: 1444216107 },
+  { name: "India", population: 1393409038 },
+  { name: "United States", population: 331002651 },
+  // Add more countries here
+];
+
+const spokenLanguages = [
+  { name: "Mandarin", speakers: 918100000 },
+  { name: "Spanish", speakers: 460000000 },
+  { name: "English", speakers: 375000000 },
+  // Add more languages here
+];
+
+function createBarChart(data, containerId, xProp, yProp) {
+  const container = document.getElementById(containerId);
+  const chart = document.createElement("div");
+  chart.classList.add("chart");
+  container.appendChild(chart);
+
+  data.forEach((item) => {
+    const bar = document.createElement("div");
+    bar.style.width = (item[yProp] / data[0][yProp]) * 100 + "%";
+    bar.textContent = `${item[xProp]} - ${item[yProp]}`;
+    chart.appendChild(bar);
+  });
+}
+
+createBarChart(populatedCountries, "population-chart", "name", "population");
+createBarChart(spokenLanguages, "language-chart", "name", "speakers");
