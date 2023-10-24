457ee3a86dce7906f3694962dac7390ef7af5605
NNT <truong06022004@gmail.com>
2023-10-24 12:23:10 +0700

    update

---
+++ Wfresh-fe/Home-page/app.js
@@ -0,0 +1,100 @@
+const carousel = document.querySelector(".carousel");
+
+let isDragStart = false,
+  prevPageX,
+  prevScrollLeft;
+
+const dragStart = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX || e.touches[0].pageX;
+  prevScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (!isDragStart) return;
+  e.preventDefault();
+  let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+  carousel.scrollLeft = prevScrollLeft - positionDiff;
+};
+const dragStop = () => {
+  isDragStart = false;
+};
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+carousel.addEventListener("mouseup", dragStop);
+carousel.addEventListener("touchend", dragStop);
+
+const carouselImg = document.querySelector(".carousel-product-hot");
+
+const dragStartImg = (e) => {
+  isDragStart = true;
+  prevPageX = e.pageX || e.touches[0].pageX;
+  prevScrollLeft = carouselImg.scrollLeft;
+};
+
+const draggingImg = (e) => {
+  if (!isDragStart) return;
+  e.preventDefault();
+  let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+  carouselImg.scrollLeft = prevScrollLeft - positionDiff;
+};
+const dragStopImg = () => {
+  isDragStart = false;
+};
+
+carouselImg.addEventListener("mousedown", dragStartImg);
+
+carouselImg.addEventListener("mousemove", draggingImg);
+
+carouselImg.addEventListener("mouseup", dragStopImg);
+
+const submitGmail = document.getElementById("submitGmail");
+const showThank = document.getElementById("showThank");
+
+submitGmail.onclick = (e) => {
+  e.preventDefault();
+  showThank.style.display = "block";
+};
+
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 4,
+//   spaceBetween: 20,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var swiper = new Swiper(".mySwiperCard", {
+  slidesPerView: 6,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
