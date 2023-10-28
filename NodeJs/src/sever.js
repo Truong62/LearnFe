3d2d059627eef510fa379ba1b3ee4b5f31e561b7
NNT <truong06022004@gmail.com>
2023-10-28 23:13:32 +0700

    nodejs

--- NodeJs/src/sever.js
+++ NodeJs/src/sever.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const path = require("path");
+require("dotenv").config();
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8888;
+const hosstname = process.env.HOST_NAME;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 app.get("/abc", (req, res) => {
-  res.send("to abc");
+  res.send("to abc dsdsd ");
 });
 
 app.listen(port, () => {
