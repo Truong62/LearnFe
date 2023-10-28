0693155cd729d63f3ba826b8da8b866501304173
NNT <truong06022004@gmail.com>
2023-10-28 14:47:14 +0700

    nodejs

---
+++ ReactJs/src/components/test.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// function Greet(){
+//     return <h1> Nguyen Ngoc Truong</h1>
+// }
+
+const Greet = () => {
+  <h1> Nguyen Ngoc Truong</h1>;
+};
+
+export default Greet;
