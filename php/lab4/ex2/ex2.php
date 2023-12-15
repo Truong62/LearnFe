6a913b145ee4ec5bc3a1b5592d45170f5cea2795
Truong62 <truong06022004@gmail.com>
2023-12-15 18:21:49 +0700

    update

--- php/lab4/ex2/ex2.php
+++ php/lab4/ex2/ex2.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <link rel="stylesheet" type="text/css" href="ex2.css">
 </head>
+
 <body>
     <div class="header-top ">
         <div class="list-tad-header ">
@@ -19,7 +20,7 @@
             <samp><i class="bi bi-fire"></i>Khuyến mãi HOT</samp>
             <samp><i class="bi bi-card-heading"></i>Vé xem phim CGV</samp>
             <samp><i class="bi bi-chat-text-fill"></i>Mua hàng nước ngoài</samp>
-            <samp><i class="bi bi-person-fill"></i>Bán hàng cùng Tiki</samp>
+            <samp><i class="bi bi-globe-americas"></i>Bán hàng cùng Tiki</samp>
         </div>
     </div>
     <div class="header-bottom">
@@ -31,42 +32,64 @@
                 <button class="btn-search">Tìm Kiếm </button>
             </form>
             <div class="tab-header-bt">
-                <h3>Theo dõi</h3>
-                <h5>đơn hàng</h5>
+                <i class="bi bi-files-alt"></i>
+                <div>
+                    <h3>Theo dõi</h3>
+                    <h5>đơn hàng</h5>
+                </div>
             </div>
             <div class="tab-header-bt">
-                <h3>Thông báo</h3>
-                <h5>của tôi</h5>
+                <i class="bi bi-bell-fill"></i>
+                <div>
+                    <h3>Thông báo</h3>
+                    <h5>của tôi</h5>
+                </div>
             </div>
             <div class="tab-header-bt">
-                <h3>Đăng nhập</h3>
-                <h5>Tài khoản</h5>
+                <i class="bi bi-person-fill"></i>
+                <div>
+                    <h3>Đăng nhập</h3>
+                    <h5>Tài khoản</h5>
+                </div>
             </div>
             <div class="cart-header">
-                <h3>Giỏ hàng</h3>
-                <span>0</span>
+                <i class="bi bi-cart-check-fill"></i>
+                <div style="display: flex; align-items: center;">
+                    <h3>Giỏ hàng</h3>
+                    <span>0</span>
+                </div>
             </div>
         </div>
     </div>
     <div class="header-bottom-2">
         <div class="tab-header-bt2">
+            <i class="bi bi-filter-right"></i>
             <h4>DANH MỤC SẢN PHẨM</h4>
         </div>
         <div class="tab-header-bt2">
+            <i class="bi bi-geo-alt-fill"></i>
             <h4>Bạn muốn giao hàng ở đâu?</h4>
         </div>
         <div class="tab-header-bt2">
+            <i class="bi bi-view-list"></i>
             <h4>Sản phẩm bạn đã xem</h4>
         </div>
         <div class="tab-header-bt2">
-            <h4>2 tiếng nhận hàng</h4>
+            <i class="bi bi-yin-yang"></i>
+           <div>
+           <h4>2 tiếng nhận hàng</h4>
             <h4>Hàng trăm nghìn sản phẩm</h4>
+           </div>
         </div>
         <div class="tab-header-bt2">
+            <i class="bi bi-yin-yang"></i>
+            <div>
             <h4>Tất cả sản phẩn </h4>
             <h4>100% chính hiệu</h4>
+            </div>
         </div>
         <div class="tab-header-bt2">
+            <i class="bi bi-yin-yang"></i>
             <h4>30 ngày đổi trả dễ dàng</h4>
         </div>
     </div>
