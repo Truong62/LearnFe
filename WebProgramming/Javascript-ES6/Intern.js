c940474020822d37881e33d3097bcc1e57d849d4
Truong62 <truong06022004@gmail.com>
2024-03-04 17:39:12 +0700

    update

--- WebProgramming/Javascript-ES6/Intern.js
+++ WebProgramming/Javascript-ES6/Intern.js
@@ -92,3 +92,88 @@ const bubbleSort = () => {
 }
 // console.log(bubbleSort())
 
+const TongBinhPhuong = (arr) => {
+    let binhphuongitem = arr.map(item => item ** 2)
+    return binhphuongitem
+
+}
+const a = TongBinhPhuong([1, 1, 1, 15, 2, 5, 5, 6, 6, 3, 4,])
+// console.log(a)
+
+const b1 = () => {
+    var arr = [[1, 2, 3], [4, 5], [6, 7, 8, 9]];
+    var arrNew = arr.map((item) => {
+        return item.reduce((acc, curr) => acc + curr, 0)
+    })
+    console.log(arrNew)
+}
+const b2 = () => {
+    var strings = ['apple', 'banana', 'pear', 'kiwi', 'orange'];
+    var objString = {}
+    strings.map((item) => {
+        var length = item.length;
+        if (objString[length]) {
+            objString[length].push(item)
+        } else {
+            objString[length] = [item];
+        }
+
+    })
+    console.log(objString)
+}
+// b2()
+
+const b3 = () => {
+    const names = ["Alice", "Bob", "Charlie", "David"];
+    const newName = names.map(item => item + " is awesome")
+    console.log(newName)
+}
+// b3()
+
+const b4 = () => {
+    const strings = ["apple", "banana", "orange", "kiwi"];
+    console.log(strings.map(item => item.length))
+}
+// b4()
+const b5 = () => {
+    const integers = [2, 4, 6, 8, 10];
+    console.log(integers.map(item => item ** 2))
+}
+// b5()
+const b6 = () => {
+    const objects = [
+        { name: "John", age: 30 },
+        { name: "Jane", age: 25 },
+        { name: "Doe", age: 40 }
+    ];
+    const newObj = objects.map((item, index) => ({
+        id: index,
+        ...item
+    }))
+    console.log(newObj)
+}
+// b6()
+const b7 = () => {
+    const strings = ["apple", "banana", "orange", "kiwi"];
+    console.log(strings.map(item => item.toUpperCase()))
+}
+
+const b8 = () => {
+    const numbers = [3.5, 1.2, 4.9, 7.8, 2.3];
+    console.log(numbers.map(item => Math.floor(item)))
+}
+// b8()
+
+const b9 = () => {
+    const numbers = [3.14159, 2.71828, 1.41421, 1.65803];
+    console.log(numbers.map(item => item.toFixed(1)))
+}
+// b9()
+
+const b10 = () => {
+    const strings = ["Lorem ipsum dolor sit amet", "consectetur adipiscing elit", "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"];
+    console.log(
+        strings.map(item => item.length > 20 ? item.slice(0, 20) + "..." : item)
+    )
+}
+b10()
